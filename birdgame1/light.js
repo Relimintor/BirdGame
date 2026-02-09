58cc874c73d4a34509749d954ba5534c920e99cf
Raptor4 <raptor4mc@gmail.com>
2026-02-09 13:40:24 +0100

    Update light.js

--- birdgame1/light.js
+++ birdgame1/light.js
@@ -1,19 +1,9 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-
-export function setupLights(scene) {
-  // Soft global light
+function setupLights(scene) {
   const ambient = new THREE.AmbientLight(0xffffff, 1.0);
   scene.add(ambient);
 
-  // Sun light
   const sun = new THREE.DirectionalLight(0xffffff, 1.2);
   sun.position.set(100, 200, 100);
   sun.castShadow = true;
-
-  sun.shadow.mapSize.width = 2048;
-  sun.shadow.mapSize.height = 2048;
-  sun.shadow.camera.near = 1;
-  sun.shadow.camera.far = 500;
-
   scene.add(sun);
 }
